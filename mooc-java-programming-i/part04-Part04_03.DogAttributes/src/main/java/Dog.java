146269b034b854dc48a3bb46c4be5db7f961de33
Jonathan <jdpooleparty@gmail.com>
2022-06-24 15:36:19 -0700

    commit2

---
+++ mooc-java-programming-i/part04-Part04_03.DogAttributes/src/main/java/Dog.java
@@ -0,0 +1,16 @@
+public class Dog{
+
+    private String name;
+    private String breed; 
+    private int age;
+
+    public Dog (String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+
+
+
+}

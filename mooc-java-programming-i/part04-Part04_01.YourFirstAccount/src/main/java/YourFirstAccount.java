64088b62bd6127e15788e366070925e66e263489
Jonathan <jdpooleparty@gmail.com>
2022-06-24 15:07:50 -0700

    commit

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,9 +4,15 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
-        Account account = new Account("Jonathan's Account", 100.0);
-        account.deposit(20.0);
-        account.saldo();
+        //instantiate an account object with the owner "Jonathan" and balance 100
+        Account account = new Account("Jonathan", 100.0);
 
+        //deposit 20
+        account.deposit(20.0);
+        
+        //view balance
+        System.out.println(account.saldo());
+
+        
     }
 }
